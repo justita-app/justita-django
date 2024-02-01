e10ee73b93e43587259ddf6bac3868799fa9aee8
Ali <a95blnd@gmail.com>
2024-02-01 17:38:27 +0330

    fix asgi

--- config/asgi.py
+++ config/asgi.py
@@ -5,12 +5,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from social.routing import websocket_urlpatterns
+
 
 
 asgi_application = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter,URLRouter
+from social.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
