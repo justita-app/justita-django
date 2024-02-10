02ebb88b0de0ebf866d009e4833584a68f318e05
heizenberg0sam <110766707+heizenberg0sam@users.noreply.github.com>
2024-02-10 18:24:58 +0330

    account bug fixes + gtm for all pages + about urls removed

--- base/urls.py
+++ base/urls.py
@@ -9,8 +9,7 @@ app_name = "base"
 urlpatterns = [
     # can work 
     path('',views.home, name='home' ),
-    path('about',views.about ,name="about"),
-    path('english-about',views.aboutEng ,name="about_eng"),
+
     path('online-attorney',views.online_attorney ,name="online-atterney"),
     path('call-counseling-lawyer' , views.ccl , name="ccl"),
 
