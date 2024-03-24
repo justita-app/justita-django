ee11d9b40ef61a3c895470a52c828d5b16325d3b
heizenberg0sam <110766707+heizenberg0sam@users.noreply.github.com>
2024-03-24 22:59:27 +0330

    dashboard edits

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('' , views.dashboardView , name='dashboard'),
-    path('allcalls' , views.dashboardViewAll , name='dashboardall'),
+    path('onlines' , views. OnliceCouncelings , name='onlines'),
     path('support/' , views.supportRoomView , name='support'),
     path('chats/' , views.ChatRoomsView , name='chats'),
     path('users/' , views.UsersView , name='users'),
